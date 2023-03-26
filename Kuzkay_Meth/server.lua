581963a33f43527695903d070796bffbd02e386f
Kenta <102387086+Kenta30@users.noreply.github.com>
2023-03-26 14:23:18 +0200

    Update server.lua

--- Kuzkay_Meth/server.lua
+++ Kuzkay_Meth/server.lua
@@ -1,7 +1,4 @@
-print("Meth car got loaded, Made by Kuzkay")
-	
-ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
 
 RegisterServerEvent('esx_methcar:start')
 AddEventHandler('esx_methcar:start', function()
@@ -10,7 +7,7 @@ AddEventHandler('esx_methcar:start', function()
 	
 	if xPlayer.getInventoryItem('acetone').count >= 5 and xPlayer.getInventoryItem('lithium').count >= 2 and xPlayer.getInventoryItem('methlab').count >= 1 then
 		if xPlayer.getInventoryItem('meth').count >= 30 then
-				TriggerClientEvent('esx_methcar:notify', _source, "~r~~h~You cant hold more meth")
+				TriggerClientEvent('esx_methcar:notify', _source, "~h~Nem fér el nálad több ~b~Meth")
 		else
 			TriggerClientEvent('esx_methcar:startprod', _source)
 			xPlayer.removeInventoryItem('acetone', 5)
@@ -20,7 +17,7 @@ AddEventHandler('esx_methcar:start', function()
 		
 		
 	else
-		TriggerClientEvent('esx_methcar:notify', _source, "~r~~h~Not enough supplies to start producing Meth")
+		TriggerClientEvent('esx_methcar:notify', _source, "~r~Nincs elég hozzávalo hogy elkészitsd a ~b~Meth-et~s~!")
 
 	end
 	
@@ -28,10 +25,10 @@ end)
 RegisterServerEvent('esx_methcar:stopf')
 AddEventHandler('esx_methcar:stopf', function(id)
 local _source = source
-	local xPlayers = ESX.GetPlayers()
-	local xPlayer = ESX.GetPlayerFromId(_source)
-	for i=1, #xPlayers, 1 do
-		TriggerClientEvent('esx_methcar:stopfreeze', xPlayers[i], id)
+	local xPlayers = ESX.GetExtendedPlayers()
+	--local xPlayer = ESX.GetPlayerFromId(_source)
+	for _, xPlayer in pairs(xPlayers) do
+		TriggerClientEvent('esx_methcar:stopfreeze', xPlayer.source, id)
 	end
 	
 end)
@@ -42,9 +39,9 @@ AddEventHandler('esx_methcar:make', function(posx,posy,posz)
 	
 	if xPlayer.getInventoryItem('methlab').count >= 1 then
 	
-		local xPlayers = ESX.GetPlayers()
-		for i=1, #xPlayers, 1 do
-			TriggerClientEvent('esx_methcar:smoke',xPlayers[i],posx,posy,posz, 'a') 
+		local Players = ESX.GetExtendedPlayers()
+		for _, Player in pairs(Players) do
+			TriggerClientEvent('esx_methcar:smoke', Player.source, posx, posy, posz, 'a') 
 		end
 		
 	else
@@ -58,7 +55,6 @@ AddEventHandler('esx_methcar:finish', function(qualtiy)
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	print(qualtiy)
 	local rnd = math.random(-5, 5)
-	TriggerEvent('KLevels:addXP', _source, 20)
 	xPlayer.addInventoryItem('meth', math.floor(qualtiy / 2) + rnd)
 	
 end)
@@ -66,11 +62,12 @@ end)
 RegisterServerEvent('esx_methcar:blow')
 AddEventHandler('esx_methcar:blow', function(posx, posy, posz)
 	local _source = source
-	local xPlayers = ESX.GetPlayers()
+	local Players = ESX.GetExtendedPlayers()
 	local xPlayer = ESX.GetPlayerFromId(_source)
-	for i=1, #xPlayers, 1 do
-		TriggerClientEvent('esx_methcar:blowup', xPlayers[i],posx, posy, posz)
+	for _, Player in pairs(Players) do
+		TriggerClientEvent('esx_methcar:blowup', Player.source, posx, posy, posz)
 	end
 	xPlayer.removeInventoryItem('methlab', 1)
 end)
+
 
